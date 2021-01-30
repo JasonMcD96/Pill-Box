eb619377c3123c2175ab7fa01007ab4b39b3e46d
chavalk <chavalk@hotmail.com>
2021-01-30 12:31:48 -0600

    Add code for GET and POST routes.

---
+++ controllers/controller.js
@@ -0,0 +1,25 @@
+// *****************************************************************************
+// controller.js - this file offers a set of routes for displaying and saving data to the db
+// *****************************************************************************
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// ===================================================================
+module.exports = function(app) {
+    
+    // GET route for getting all patients
+    app.get("/api/patients", function(req, res) {
+        db.Patient.findAll().then(function(dbPatients) {
+            res.render("index", dbPatients);
+        });
+    });
+
+    // POST route for saving a new patient
+    app.post("/api/patients", function(req, res) {
+        db.Patient.create(req.body).then(function(dbPatient) {
+            res.json(dbPatient);
+        });
+    });
+};
