c68b1e29553b85985e1776aa7a9c4a1eff88f82b
Jason McDonald <jasonmcd65@gmail.com>
2021-02-02 20:25:37 -0600

    sign up sends information across

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
-    $("#submitButton").on("click", function(event){
+    $("#loginSubmit").on("click", function(event){
         event.preventDefault();
         console.log('You clicked it!')
     })
 
+    $("#signUpSubmit").on("click", function(event){
+        event.preventDefault();
+        console.log("You clicked sign up!")
+        let emailInput = $("input#email")
+        let passwordInput = $("input#password")
 
+        let userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim()
+        }
+
+        console.log("Data Test: ",userData)
+        // send ajax post request
+        $.ajax("/api/signup", {
+            type: "POST",
+            data: userData
+          }).then(
+            function() {
+              console.log("created user");
+              // Reload the page to get the updated list
+              location.reload();
+            }
+          );
+    })
 
 })
