93ac8a189470537e9f69777f1075482c5832a704
Jason McDonald <jasonmcd65@gmail.com>
2021-02-02 20:48:04 -0600

    sends information for login

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,36 @@
-$(document).ready(function() {
-    $("#loginSubmit").on("click", function(event){
+$(document).ready(function () {
+    $("#loginSubmit").on("click", function (event) {
         event.preventDefault();
         console.log('You clicked it!')
+        let emailInput = $("input#email")
+        let passwordInput = $("input#password")
+
+        var userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        if (!userData.email || !userData.password) {
+            return;
+        }
+
+        emailInput.val("");
+        passwordInput.val("");
+
+        $.ajax("/api/login", {
+            type: "POST",
+            data: userData
+        }).then(function () {
+
+            window.location.replace("/dashboard");
+            // If there's an error, log the error
+        }).catch(function (err) {
+            console.log(err);
+        });
+
     })
 
-    $("#signUpSubmit").on("click", function(event){
+    $("#signUpSubmit").on("click", function (event) {
         event.preventDefault();
         console.log("You clicked sign up!")
         let emailInput = $("input#email")
@@ -15,18 +41,24 @@ $(document).ready(function() {
             password: passwordInput.val().trim()
         }
 
-        console.log("Data Test: ",userData)
+        if (!userData.email || !userData.password) {
+            return;
+        }
+
+        emailInput.val("");
+        passwordInput.val("");
+
         // send ajax post request
         $.ajax("/api/signup", {
             type: "POST",
             data: userData
-          }).then(
-            function() {
-              console.log("created user");
-              // Reload the page to get the updated list
-              location.reload();
+        }).then(
+            function () {
+                console.log("created user");
+                // Reload the page to get the updated list
+                location.reload();
             }
-          );
+        );
     })
 
 })
