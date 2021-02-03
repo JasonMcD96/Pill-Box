ff95ab8974d055f671d1e2ee638ee15be497d2d0
Gabriele Falloon <ggfalloon@gmail.com>
2021-02-02 18:54:19 -0600

    added api routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,22 +4,74 @@
 
 // Requiring our models
 var db = require("../models");
+var passport = require("../config/passport");
 
 // Routes
 // ===================================================================
-module.exports = function(app) {
-    
+module.exports = function (app) {
+
+    // Initial log in route
+    app.get("/", function (req, res) {
+        res.render("index");
+    });
+
+    // GET route for create user page
+    app.get("/createuser", function (req, res) {
+        res.render("createUser");
+    });
+
     // GET route for getting all patients
-    app.get("/api/patients", function(req, res) {
-        db.Patient.findAll().then(function(dbPatients) {
-            res.render("index", dbPatients);
+    app.get("/api/patients", function (req, res) {
+        db.Patient.findAll().then(function (dbPatients) {
+            res.render("", dbPatients);
         });
     });
 
-    // POST route for saving a new patient
-    app.post("/api/patients", function(req, res) {
-        db.Patient.create(req.body).then(function(dbPatient) {
+    // GET rout for finding one patient
+    app.get("/api/patients/:id", function (req, res) {
+        db.Patient.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbPatient) {
             res.json(dbPatient);
         });
     });
+
+
+    // POST route for saving a new patient
+    app.post("/api/patient", function (req, res) {
+        db.Patient.create(req.body).then(function (dbPatient) {
+            res.json(dbPatient);
+        });
+    });
+
+
+
+
+
+    app.post("/api/login", passport.authenticate("local"), function (req, res) {
+        res.json(req.user);
+    });
+
+    app.post("/api/signup", function (req, res) {
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(function () {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function (err) {
+                res.status(401).json(err);
+            });
+    });
+
+    // Route for logging user out
+    app.get("/logout", function (req, res) {
+        req.logout();
+        res.redirect("/");
+    });
+
+
 };
