0fabb9b47a6decbf05a27f716996d1b4b5abfaa9
Gabriele Falloon <ggfalloon@gmail.com>
2021-02-04 19:09:24 -0600

    added Meds model

---
+++ models/meds.js
@@ -0,0 +1,46 @@
+modules.exports = function (sequelize, DataTypes) {
+    var Meds = sequelize.define("Meds", {
+        med_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        dosage: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        timesPD: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        withFood: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        taken: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        notes: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        }
+    });
+
+    Meds.associate = function (models) {
+
+        Meds.belongsTo(models.Patient, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Meds;
+}
--- models/patient.js
+++ models/patient.js
@@ -6,30 +6,6 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 len: [1]
             }
-        },
-        medication: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        timesPD: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        withFood: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        taken: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        notes: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            len: [1]
         }
     });
 
