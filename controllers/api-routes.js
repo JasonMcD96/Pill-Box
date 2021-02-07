3d9352d25e2bb211c7caf8da8a48d34728ae5e83
chavalk <chavalk@hotmail.com>
2021-02-07 11:19:33 -0600

    Added post route to add medication.

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -35,7 +35,7 @@ module.exports = function (app) {
         res.redirect("/");
     });
 
-    // POST route for saving a new patient
+    // POST route for adding new patient
     app.post("/api/patient", function (req, res) {
         db.Patient.create({
             name: req.body.name,
@@ -45,6 +45,20 @@ module.exports = function (app) {
         });
     });
 
+    // POST route for adding new medication
+    app.post("/api/medication", function (req, res) {
+        db.Meds.create({
+            med_name: req.body.med_name,
+            dosage: req.body.dosage,
+            timesPD: req.body.timesPD,
+            withFood: req.body.withFood,
+            taken: req.body.taken,
+            notes: req.body.notes,
+            PatientId: req.body.PatientId,
+        }).then(function (dbMed) {
+            res.json(dbMed);
+        })
+    })
     // Route for getting data about user to be used on client side
     app.get("/api/user_data", function (req, res) {
         if (!req.user) {
